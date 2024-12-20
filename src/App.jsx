46f133495869a50ab0c753ba0a9bc65f2091cbf9
Fernando Herrera <herrera770@outlook.es>
2024-12-20 18:32:00 -0300

    se creó un diseño básico de la página de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import BarraNavegacion from './common/navbar/BarraNavegacion'
+import Inicio from './pages/inicio/inicio'
 import './styles/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -8,11 +9,7 @@ function App() {
   return (
     <>
     <BarraNavegacion></BarraNavegacion>
-      <div>
-        <h1>Esta página funciona</h1>
-        <h2>Y ahora tiene bootstrap</h2>
-        <h3>Ademas las fuentes funcionan</h3>
-      </div>
+    <Inicio></Inicio>
     </>
   )
 }
