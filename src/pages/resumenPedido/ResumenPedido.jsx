61b11100d5b20eeb4ba149f217dddfa1c3a36f3b
Fernando Herrera <herrera770@outlook.es>
2024-12-30 21:25:06 -0300

    agregado el resumen del menu fixed

--- src/pages/resumenPedido/ResumenPedido.jsx
+++ src/pages/resumenPedido/ResumenPedido.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './resumenPedido.css'
 import { Container, ListGroup } from 'react-bootstrap';
+import MenuResumenPedido from './components/MenuResumenPedido';
 
 const ResumenPedido = () => {
     return (
       <>
+      <MenuResumenPedido></MenuResumenPedido>
        <div className="d-flex py-4 px-4 bannerPagina">
         <a href="#">
           <i className="bi bi-chevron-left text-light fs-1"></i>
@@ -12,7 +14,7 @@ const ResumenPedido = () => {
         <p className="text-center flex-grow-1 fs-1 mb-0 text-light"> Resumen  </p>
       </div>
       <Container className='p-3'>
-        <ListGroup className=' border border-secondary'>
+        <ListGroup className=' border border-secondary mb-5'>
             <div className='d-flex justify-content-between py-1 px-1 border-bottom border-dark'>
                 <div><p className='mb-0'>Juan Perez</p></div>
                 <div><p className='mb-0'>Tel: 3811234567</p></div>
