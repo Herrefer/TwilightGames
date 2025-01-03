aa951101d5505becaef2ca541a730163a5dad106
Fernando Herrera <herrera770@outlook.es>
2025-01-03 13:46:25 -0300

    se agregaron las validaciones necesarias para el formulario de pedido

--- src/pages/formularioPedido/FormularioPedido.jsx
+++ src/pages/formularioPedido/FormularioPedido.jsx
@@ -3,8 +3,18 @@ import "./formularioPedido.css";
 import { Button, Container, Form } from "react-bootstrap";
 import Banner from "../../assets/bannerBackground.png";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const FormularioPedido = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isDirty },
+  } = useForm();
+
+  const onSubmit = (datos) => console.log(datos);
+
   return (
     <>
       {/* aqui revisar si es mejor un margin 4 o 5 (o tal vez más pequeño) */}
@@ -12,34 +22,95 @@ const FormularioPedido = () => {
         <Link to={"/juegos"}>
           <i className="bi bi-chevron-left text-light fs-1"></i>
         </Link>
-        <p className="text-center flex-grow-1 fs-1 mb-0 text-light"> Datos de contacto </p>
+        <p className="text-center flex-grow-1 fs-1 mb-0 text-light">
+          {" "}
+          Datos de contacto{" "}
+        </p>
       </div>
       <Container fluid className="contenedorPagina">
-        <Form className="p-3">
+        <Form onSubmit={handleSubmit(onSubmit)} className="p-3">
           <Form.Group className="mb-3" controlId="formNombre">
             <Form.Label>Apellido y nombre</Form.Label>
             <Form.Control
               type="text"
               placeholder="Ej: Perez Juan"
+              {...register("apellidoNombre", {
+                required: "campo obligatorio",
+                minLength: {
+                  value: 7,
+                  message: "el campo debe tener un mínimo de 7 caracteres",
+                },
+                maxLength: {
+                  value: 50,
+                  message: "el campo debe tener un máximo de 50 caracteres",
+                },
+              })}
             ></Form.Control>
+            {errors.apellidoNombre ? (
+              <Form.Text className="text-danger ms-1">
+                {errors.apellidoNombre?.message}
+              </Form.Text>
+            ) : (
+              <Form.Text className="ms-1">campo obligatorio</Form.Text>
+            )}
           </Form.Group>
           <Form.Group className="mb-3" controlId="formNumero">
             <Form.Label>Número de teléfono móvil</Form.Label>
             <Form.Control
-              type="tel"
+              type="number"
               placeholder="Ej: 3811234567"
+              {...register("numeroTel", {
+                required: "campo obligatorio",
+                minLength: {
+                  value: 10,
+                  message: "el número debe tener un mínimo de 10 caracteres",
+                },
+                maxLength: {
+                  value: 10,
+                  message: "el número debe tener un máximo de 10 caracteres",
+                },
+              })}
             ></Form.Control>
+            {errors.numeroTel ? (
+              <Form.Text className="text-danger ms-1">
+                {errors.numeroTel?.message}
+              </Form.Text>
+            ) : (
+              <Form.Text className="ms-1">campo obligatorio</Form.Text>
+            )}
           </Form.Group>
           <Form.Group className="mb-3" controlId="formCorreo">
             <Form.Label>Correo electrónico</Form.Label>
             <Form.Control
               type="email"
               placeholder="Ej: juanperez@correo.com"
+              {...register("correo", {
+                required: "campo obligatorio",
+                minLength: {
+                  value: 3,
+                  message: "el correo debe tener un mínimo de 3 caracteres",
+                },
+                maxLength: {
+                  value: 265,
+                  message: "El correo debe tener un máximo de 265 caracteres",
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                  message: "Ingrese una dirección de correo electrónico válida",
+                },
+              })}
             ></Form.Control>
+            {errors.correo ? (
+              <Form.Text className="text-danger ms-1">
+                {errors.correo?.message}
+              </Form.Text>
+            ) : (
+              <Form.Text className="ms-1">campo obligatorio</Form.Text>
+            )}
           </Form.Group>
           <div className="text-center">
             <Button variant="secondary" type="submit">
-              Continuar al resumen <i class="bi bi-chevron-right"></i>
+              Continuar al resumen <i className="bi bi-chevron-right"></i>
             </Button>
           </div>
         </Form>
