fa13b7db58dfaf7bcf6390288b794d716768ddf8
Fernando Herrera <herrera770@outlook.es>
2025-01-09 17:59:13 -0300

    en el MenuCarrito, el div JUEGOS ahora es un button y fué estilizado

--- src/pages/listaJuegos/components/MenuCarrito.jsx
+++ src/pages/listaJuegos/components/MenuCarrito.jsx
@@ -4,20 +4,22 @@ import { Link } from "react-router-dom";
 
 const MenuCarrito = ({ contadorJuegosPedidoProp }) => {
   return (
-    <div className="pedidoContainer position-fixed bottom-0 d-flex justify-content-between align-items-center">
-      <div>
-        <p className="mb-0 ms-2 fs-5">X{contadorJuegosPedidoProp} JUEGOS</p>
-      </div>
-      <div className="d-flex flex-column align-items-center">
-        <p className="m-0 fs-5">$5500</p>
-        <p className="m-0 fs-6 fw-light">precio final</p>
-      </div>
-      <Link className="botonPedidoContainer" to={"/formulario"}>
-        <button className="botonPedido rounded-start-pill fs-5">
-          Al pedido
+    <>
+      <div className="pedidoContainer position-fixed bottom-0 d-flex justify-content-between align-items-center">
+        <button className="botonMostrarListado rounded-end-pill">
+          <p className="mb-0 fs-5">X{contadorJuegosPedidoProp} JUEGOS</p>
         </button>
-      </Link>
-    </div>
+        <div className="d-flex flex-column align-items-center">
+          <p className="m-0 fs-5">$5500</p>
+          <p className="m-0 fs-6 fw-light">precio final</p>
+        </div>
+        <Link className="botonPedidoContainer" to={"/formulario"}>
+          <button className="botonPedido rounded-start-pill fs-5">
+            Al pedido
+          </button>
+        </Link>
+      </div>
+    </>
   );
 };
 
